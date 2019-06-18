0987551b166a7ed10e56a96c7297426420a90c1c
Max Pshenichniy <pml06011998@gmail.com>
2019-06-18 21:23:50 +0300

    corect js test

--- js-test-task/js/script.js
+++ js-test-task/js/script.js
@@ -22,22 +22,29 @@ var rows = [{
     var sum = 0;
 
     	html+="<tr>";
-        html+="<th>"+"Имя"+"</td>";
-        html+="<th>"+"Возраст"+"</td>";
-
+        html+="<th>"+"Имя"+"</th>";
+        html+="<th>"+"Возраст"+"</th>";
+        html+="<th>"+"Количество людей с таким же именем"+"</th>";
         html+="</tr>";
        
 
     for (var i = 0; i < rows.length; i++) {
+       
+
+        var counter = 0;
+        for (var j = 0; j < rows.length; j++){
+            if(rows[i].name === rows[j].name) {
+                counter++;
+            }
+        }
+
         html+="<tr>";
         html+="<td>"+rows[i].name+"</td>";
         html+="<td>"+rows[i].age+"</td>";
-        
-
+        html+="<td>"+counter+"</td>";
         html+="</tr>";
 
         sum += rows[i].age;
-
     }
 
     html+="</table>";

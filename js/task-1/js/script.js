3867f3d2bdbaa288493c63e3a024eac95be13707
Max Pshenichniy <pml06011998@gmail.com>
2019-06-29 19:55:48 +0300

    js/task-1

--- js/task-1/js/script.js
+++ js/task-1/js/script.js
@@ -1,48 +1,23 @@
 
-function* generator(user) {
-	var a, b, start = 0, sum; 
-	if (user.flag === 'yes'){
-		yield start;
-		a = 1;
-		b = 1;
-		yield a;
-		yield b;
-		for(var i = 3; i <= user.n; i++){
-			sum = a + b;
-			a = b;
-			b = sum;
-			yield b;
-		}
-	} 
-	else {
-		a = 1;
-		b = -1;
-		yield start;
-		yield a;
-		yield b;
-		for(var i = 3; i <= user.n; i++){
-			sum = a - b;
-			a = b;
-			b = sum;
-			yield b;
-		}
-	}
-	
-	
+function* fibo() {
+  	[a, b] = [0, 1]
+  	while (true) {
+    	yield a;
+    	[a, b] = [b, a + b]
+  	}
 }
 
 
-
 do {
-	
-	var user = {
-		n: prompt("Введите число:", 7),
-		flag: prompt("Введите направления ряда: yes - положительные, другие отрицательные", 'yes')
+	var n = prompt("Введите число:")
+
+	var fib = fibo();
+
+
+
+	for (let i = 0; i < n; i++) {
+  		console.log(fib.next().value);
 	}
-
-	var sequence = [...generator(user)];
-
-	alert (sequence); 
 
 	g = prompt("Хотите продолжить работу?", 'yes')
 } while (g === 'yes')
